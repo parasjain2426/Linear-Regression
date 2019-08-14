35e9f4b7d91740df287ba6f23da16bc74feea3d6
Paras Jain <paras.2426@gmail.com>
2019-08-14 18:32:03 +0530

    Update test2.m

--- test2.m
+++ test2.m
@@ -1,7 +1,7 @@
-x=load('testdata.txt');
-input=x(:,5);
-y=x(:,3);
-normalization= true;
+x=load('testdata.txt');%upload the training data here
+input=x(:,end-1);
+y=x(:,end);
+normalization= true; %normalizing the training data
 if (normalization)
     max1=max(input);
     min1=min(input);
@@ -9,12 +9,12 @@ if (normalization)
 end
 i=[ones(length(input),1) input];
 theta=zeros(2,1);
-alpha=0.01;
-iterations=1500;
+alpha=0.01; %learning rate
+iterations=1500; 
 m=length(y);
 [theta, Jhis]=update(i,y,theta,alpha,iterations,m);
 y1=25;
-if(normalization)
+if(normalization)  %normalizing the testing data with max and min terms of trained data
 y1=(y1-max1)/(max1-min1);
 end
 ypred=theta(1)+theta(2)*y1
